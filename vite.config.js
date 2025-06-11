2f533ed7133b45f052fb5585580ddc5fd8cce38b
zloy <ilya.sidorovich006@gmail.com>
2025-06-12 01:33:49 +0300

    ending main page

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 export default defineConfig({
     root: './src',
-    base: '/coca/',
+    base: '/stroikom/',
     server: {
         port: 3000,
     },
