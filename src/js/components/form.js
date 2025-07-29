dc9c3df7dae89c045b4657b8c3d158e35e2a8bd7
zloy <ilya.sidorovich006@gmail.com>
2025-07-29 23:57:29 +0300

    added email and form

--- src/js/components/form.js
+++ src/js/components/form.js
@@ -1,36 +1,50 @@
 export function formVisible() {
-    const formButton = document.querySelectorAll('.form__button')
+    const formButton = document.querySelectorAll('.form__button');
     const formItem = document.querySelector('.form');
-    const burgerShadow = document.querySelector('.burger_shadow')
-    const bodyClass = document.querySelector('.body')
-
+    const formShadow = document.querySelector('.form__shadow');
+    const bodyClass = document.querySelector('.body');
+    const burgerItem = document.querySelector('.burger');
     formButton.forEach(btn => {
         btn.addEventListener('click', () => {
-            formItem.style.zIndex = '13';
+            formItem.style.zIndex = '17';
             formItem.style.left = '35vw';
             formItem.style.pointerEvents = 'auto';
             bodyClass.style.overflow = 'hidden'
-            burgerShadow.style.display = 'block';
-            burgerShadow.style.zIndex = '12';
-            burgerClass.style.pointerEvents = 'all';
+            formShadow.style.display = 'block';
+            burgerItem.style.pointerEvents = 'none';
         })
     }
     )
 
 }
 export function formAnvisible(){
-    const formButton = document.querySelector('.form__cancel')
+    const formButton = document.querySelector('.form__cancel');
     const formItem = document.querySelector('.form');
-    const burgerShadow = document.querySelector('.burger_shadow')
-    const bodyClass = document.querySelector('.body')
+    const formShadow = document.querySelector('.form__shadow');
+    const bodyClass = document.querySelector('.body');
+    const burgerItem = document.querySelector('.burger');
         formButton.addEventListener('click', () => {
             bodyClass.style.overflow = 'auto'
             formItem.style.zIndex = '-1';
             formItem.style.left = '-150vw';
             formItem.style.pointerEvents = 'none';
-            burgerShadow.style.display = 'none';
-            burgerClass.style.pointerEvents = 'auto';
-            burgerShadow.style.zIndex = '10';
+            formShadow.style.display = 'none';
+            burgerItem.style.pointerEvents = 'auto';
         })
+}
 
+export function sendingForm(){
+    (function(){
+        emailjs.init("BJiCliBmgnFHOXcXq");  // Replace with your EmailJS user ID
+    })();
+
+    document.getElementById("form").addEventListener("submit", function(event) {
+        event.preventDefault();
+        emailjs.sendForm('service_3i86326', 'template_c1i0j2b', this)
+            .then(function() {
+                alert('Email sent successfully!');
+            }, function(error) {
+                alert('Failed to send email: ' + JSON.stringify(error));
+            });
+    });
 }
--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,7 @@ import '/scss/main.scss';
 // components
 
 import { burgerCancel, headerMinActiveded, burgerTransition,burgerMainOpened,burgerTabletOpened } from './components/header.js';
-import {formVisible, formAnvisible} from './components/form.js';
+import {formVisible, formAnvisible, sendingForm} from './components/form.js';
 burgerTabletOpened();
 headerMinActiveded();
 burgerMainOpened();
@@ -11,4 +11,5 @@ burgerCancel();
 burgerTransition();
 formVisible();
 formAnvisible();
+sendingForm();
 
